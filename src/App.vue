2cf8d5bbc54766cdd35152c1556f0a08bb180d29
Samuel Magafu <hi@sammagafu.com>
2023-10-03 16:35:52 +0300

    fixed navbar issues

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import TopNavBar from '@/components/TopNavBar.vue'
-import Footer from '@/components/Footer.vue';
 import TopNavBar from '@/components/TopNavBar.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
