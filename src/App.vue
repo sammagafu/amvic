5330528e1bffd55a687f6ecad16712087c896a67
Samuel Magafu <hi@sammagafu.com>
2023-10-03 16:43:13 +0300

    fixed the navbar with new one

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
-import Topheader from './components/Topheader.vue';
+import Topheader from '@/components/Topheader.vue';
 import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
   <div class="navbar">
-    <Topheader/>
+    <Topheader />
   </div>
   <RouterView />
   <div class="footer">
