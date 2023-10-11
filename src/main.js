6bfb46fa3b17183a9826404f4ad9ea21fa4ba9f3
Samuel Magafu <hi@sammagafu.com>
2023-10-11 03:45:15 +0300

    fixed navbar issue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueWriter from "vue-writer";
-import vWow from 'v-wow'
 
 
 
@@ -15,5 +14,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueWriter)
-app.use(vWow);
 app.mount('#app')
